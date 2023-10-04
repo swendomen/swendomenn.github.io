2c0680ec3e7f42e9a0c8ef9ddb40b1c0e9e07db2
jet widjaya <getdisjett@gmail.com>
2023-10-04 08:13:51 +0700

    first commit

---
+++ dejs.js
@@ -0,0 +1,21 @@
+ScrollReveal({
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".kikiki", {
+  origin: "left",
+});
+
+ScrollReveal().reveal(".kakaka", {
+  origin: "right",
+});
+
+ScrollReveal().reveal(".tatata", {
+  origin: "top",
+});
+
+ScrollReveal().reveal(".wawawa", {
+  origin: "bottom",
+});
